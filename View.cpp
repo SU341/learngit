843cbe7915a1ea88561531d88fa83ae09300bbff
417 <417555802@qq.com>
2022-02-22 13:13:36 +0800

    feat:add View.cpp

---
+++ View.cpp
@@ -0,0 +1,23 @@
+﻿#include <QFileDialog>
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QWheelEvent>
+#include "UI/Common/LoadingDialog.h"
+#include "Common.h"
+#include <qmath.h>
+#include <QPolygon>
+#include <QScrollBar>
+#include <thread>
+
+ModelingGraphicsView::ModelingGraphicsView(QWidget *parent):
+    QGraphicsView(parent),
+    m_scrolling(false)
+{
+    reset();
+    //	反锯齿
+    setRenderHints(QPainter::Antialiasing);
+    //	隐藏滚动条
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+}
+
